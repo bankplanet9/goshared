9044e8c46b2c0f2cdd9e08de073c700ece0a09db
Avesto Innovations <avesto.inn@gmail.com>
2023-10-30 16:41:19 +0400

    bugs fixed on field names

--- dto/scoring.go
+++ dto/scoring.go
@@ -3,5 +3,5 @@ package dto
 // ForecastLimitAssigned is a body payload for message coming from SCORING.forecast_limit_assigned topic
 type ForecastLimitAssigned struct {
 	ClientID *string  `validate:"required" json:"client_id"`
-	Limit    *float64 `validate:"required" json:"limit"`
+	LimitAmount    *float64 `validate:"required" json:"limit_amount"`
 }
