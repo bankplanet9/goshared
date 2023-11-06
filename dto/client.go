399db7aba60357db58733d81b1f556e28320362b
Avesto Innovations <avesto.inn@gmail.com>
2023-11-06 18:45:21 +0400

    client IsPreApproved universal method added

--- dto/client.go
+++ dto/client.go
@@ -5,15 +5,24 @@ import "time"
 type Client struct {
 	ID string `json:"id" validate:"required" gorm:"primaryKey"` // ID клиента
 
+	// IsIdentified shows whether the client is identified or not
+	IsIdentified bool `json:"is_identified"` // Идентифицирован ли клиент
+
 	// Scoring
-	Limit           *float64   `json:"limit"`             // Сумма лимита
-	LimitAssignedAt *time.Time `json:"limit_assigned_at"` // Дата и время определения лимита
+	Limit            *float64   `json:"limit"`              // Сумма лимита
+	LimitAssignedAt  *time.Time `json:"limit_assigned_at"`  // Дата и время определения лимита
 	LimitRequestedAt *time.Time `json:"limit_requested_at"` // Дата и время запроса на определение лимита
 
 	// Credit History
-	HasGoodCreditHistory   bool       `json:"has_good_credit_history"`   // Индикатор качества кредитной истории (хорошая, плохая). Реализация определена микросервисом credit-history
+	HasGoodCreditHistory     *bool      `json:"has_good_credit_history"`     // Индикатор качества кредитной истории (хорошая, плохая). Реализация определена микросервисом credit-history
 	CreditHistoryRequestedAt *time.Time `json:"credit_history_requested_at"` // Дата и время запроса на проверку кредитной истории
-	CreditHistoryCheckedAt *time.Time `json:"credit_history_checked_at"` // Дата и время осуществления проверки кредитной истории
+	CreditHistoryCheckedAt   *time.Time `json:"credit_history_checked_at"`   // Дата и время осуществления проверки кредитной истории
+
+	// Quota related
+	// Quota owners are customers with bad credit history, but Planet9 assigns them a limit up to X amount,
+	// defined in system configuration to give them a change to change their credit history
+	HasQuotaAssigned *bool      `json:"has_quota_assigned"` // Получил квоту будучи плохим клиентом (да, нет)
+	QuotaAssignedAt  *time.Time `json:"quota_assigned_at"`  // Дата и время назначения квоты
 
 	RemittanceLogs []RemittanceLog `json:"-"`
 
@@ -24,11 +33,23 @@ func (Client) TableName() string {
 	return "clients_clients"
 }
 
-// IsGood determines whether the client is good according to these factors:
-//
-// 1. A client has limit
-//
-// 2. A client has a good credit history
-func (c Client) IsGood() bool {
-	return c.HasGoodCreditHistory && c.Limit != nil
+// IsPreApproved determines whether we can provide a loan to the client or not.
+func (c Client) IsPreApproved() bool {
+	// If the client is not identified, they must at least have a limit to be pre-approved.
+	if !c.IsIdentified && c.Limit != nil {
+		return true
+	}
+
+	// If the client is identified and has a good credit history and limit assigned, they are pre-approved.
+	if c.IsIdentified && c.HasGoodCreditHistory != nil && *c.HasGoodCreditHistory && c.Limit != nil {
+		return true
+	}
+
+	// If the client is identified and has a bad credit history with a quota & limit assigned, they are pre-approved.
+	if c.IsIdentified && c.HasGoodCreditHistory != nil && !*c.HasGoodCreditHistory && *c.HasQuotaAssigned && c.Limit != nil {
+		return true
+	}
+
+	// Default case: not pre-approved.
+	return false
 }
---
+++ dto/client_test.go
@@ -0,0 +1,64 @@
+package dto
+
+import (
+	"testing"
+)
+
+func TestClientIsPreApproved_GoodCHAndLimit(t *testing.T) {
+	hasGoodCreditHistory := true
+	limit := 2000.00
+	c := &Client{
+		IsIdentified:         true,
+		HasGoodCreditHistory: &hasGoodCreditHistory,
+		Limit:                &limit,
+	}
+
+	if !c.IsPreApproved() {
+		t.Fatal("User has good credit history & limit assigned but has been not pre-approved")
+	}
+}
+
+func TestClientIsPreApproved_BadClientButQuotaAssigned(t *testing.T) {
+	hasGoodCreditHistory := false
+	HasQuotaAssigned := true
+	limit := 1000.00
+	c := &Client{
+		IsIdentified:         true,
+		HasGoodCreditHistory: &hasGoodCreditHistory,
+		Limit:                &limit,
+		HasQuotaAssigned:     &HasQuotaAssigned,
+	}
+
+	if !c.IsPreApproved() {
+		t.Fatal("User has bad credit history and quota assigned but has been not pre-approved")
+	}
+}
+
+func TestClientIsPreApproved_BadClientNotQuotaAssigned(t *testing.T) {
+	hasGoodCreditHistory := false
+	HasQuotaAssigned := false
+	limit := 1000.00
+	c := &Client{
+		IsIdentified:         true,
+		HasGoodCreditHistory: &hasGoodCreditHistory,
+		Limit:                &limit,
+		HasQuotaAssigned:     &HasQuotaAssigned,
+	}
+
+	if c.IsPreApproved() {
+		t.Fatal("User has bad credit history and has not quota assigned but has been pre-approved")
+	}
+}
+
+func TestClientIsPreApproved_NotIdentified(t *testing.T) {
+	limit := 1000.00
+	c := &Client{
+		IsIdentified:         true,
+		HasGoodCreditHistory: nil,
+		Limit:                &limit,
+	}
+
+	if c.IsPreApproved() {
+		t.Fatal("User is not identified, has a limit assigned but has not been pre-approved")
+	}
+}
