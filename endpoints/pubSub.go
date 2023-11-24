fa4349baf4b18b792dcd99aba64cba355fc52ed1
Avesto Innovations <avesto.inn@gmail.com>
2023-11-24 16:21:08 +0400

    client updated endpoint added

--- endpoints/pubSub.go
+++ endpoints/pubSub.go
@@ -10,6 +10,7 @@ type PubSubEndpoint = string
 const (
 	RemittanceCreateRemittanceLog PubSubEndpoint = "REMITTANCE.create_remittance_log"
 	RemittanceCreateRemittanceLogsBatch PubSubEndpoint = "REMITTANCE.create_remittance_logs_batch"
+	RemittanceClientUpdated PubSubEndpoint = "REMITTANCE.client_updated"
 	ScoringForecastLimitAssigned  PubSubEndpoint = "SCORING.forecast_limit_assigned"
 	CreditHistoryCheck PubSubEndpoint = "CREDIT_HISTORY.check"
 	CreditHistoryChecked PubSubEndpoint = "CREDIT_HISTORY.checked"
