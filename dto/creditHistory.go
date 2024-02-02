e86ba5ee68a0f294beb825973d8ef18a304117db
bekha-io <avesto.inn@gmail.com>
2024-02-02 15:41:39 +0400

    creditHistoryLog now has validity parameters

--- dto/creditHistory.go
+++ dto/creditHistory.go
@@ -1,5 +1,6 @@
 package dto
 
+
 // CreditHistoryLog represents an ORM model for credit history checks stored in DB
 type CreditHistoryLog struct {
 	ID       uint   `json:"id" gorm:"primaryKey"` // ID записи в базе данных
@@ -8,6 +9,10 @@ type CreditHistoryLog struct {
 	ReportID          *string `json:"report_id" gorm:"unique"` // ID отчета кредитной истории в системе провайдера кредитной истории
 	IsBadBorrower     *bool   `json:"is_bad_client"`           // Плохой заёмщик (binary)
 	BadBorrowerReason *string `json:"bad_borrower_reason"`     // Критерии, по которым клиент был определен как плохой заёмщик
+
+	// Validity
+	IsValid *bool `json:"is_valid"` // Флаг валидности клиента
+	InvalidReason *string `json:"invalid_reason"` // Причина, по которой клиент не считается валидным
 
 	CreatedUpdatedMixin
 }
